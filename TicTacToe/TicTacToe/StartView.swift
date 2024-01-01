d4b7c1b3e19f0280ee27bc096d6d55175b8e1167
wiz-boc <wiz_kidz@outlook.com>
2024-01-01 10:58:00 -0500

    Create launchScreen and setup startScreen

---
+++ TicTacToe/TicTacToe/StartView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by wizz on 1/1/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    StartView()
+}
---
+++ TicTacToe/TicTacToe/Util/ViewModifiers.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModifiers.swift
+//  TicTacToe
+//
+//  Created by wizz on 1/1/24.
+//
+
+import Foundation
